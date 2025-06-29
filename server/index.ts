9118422cbcc7e5f8ec78dd3392e05fd6960419e2
Alex Otero <38643310-alejotero31@users.noreply.replit.com>
2025-06-29 14:40:38 +0000

    Going back to client savng properties as favorite

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { initEmailService } from "./emailService";
-import { type Server } from "http";
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
